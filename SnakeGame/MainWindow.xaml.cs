855347f6f4c6865dc8c3a3a4a49817f8c8ff9f75
margin2031 <serbakovd187@gmail.com>
2025-06-02 23:13:59 +0300

    примерно так

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -16,9 +16,48 @@ namespace SnakeGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly SnakeGameViewModel viewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+            viewModel = new SnakeGameViewModel();
+            DataContext = viewModel;
+
+            CompositionTarget.Rendering += RenderGame;
+        }
+
+        private void RenderGame(object sender, System.EventArgs e)
+        {
+            GameCanvas.Children.Clear();
+
+            foreach (var part in viewModel.SnakeParts)
+            {
+                var rect = new Rectangle
+                {
+                    Width = SnakeGameModel.SnakeSize,
+                    Height = SnakeGameModel.SnakeSize,
+                    Fill = Brushes.Green
+                };
+                Canvas.SetLeft(rect, part.X);
+                Canvas.SetTop(rect, part.Y);
+                GameCanvas.Children.Add(rect);
+            }
+
+            var foodRect = new Rectangle
+            {
+                Width = SnakeGameModel.SnakeSize,
+                Height = SnakeGameModel.SnakeSize,
+                Fill = Brushes.Red
+            };
+            Canvas.SetLeft(foodRect, viewModel.FoodPosition.X);
+            Canvas.SetTop(foodRect, viewModel.FoodPosition.Y);
+            GameCanvas.Children.Add(foodRect);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Keyboard.Focus(this); // нужно, чтобы InputBindings работали
         }
     }
 }
