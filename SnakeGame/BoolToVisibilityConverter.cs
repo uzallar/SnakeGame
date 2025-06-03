e40369373a6f9505931049c278d4a3e64fb6ede6
margin2031 <serbakovd187@gmail.com>
2025-06-03 22:28:53 +0300

    полная логика

---
+++ SnakeGame/BoolToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace SnakeGame
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool boolVal && boolVal ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Visibility vis && vis == Visibility.Visible;
+        }
+    }
+}
