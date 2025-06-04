9e5f8294344ff1dd2fdeec5e2fa7e56b9d6db8bf
uzallar <k.malvina555@gmail.com>
2025-06-04 12:04:47 +0300

    тесты

--- Tests1/IntegrationTest1.cs
+++ Tests1/IntegrationTest1.cs
@@ -5,7 +5,7 @@ using Xunit;
 using SnakeGame;
 
 
-namespace Tests1 
+namespace Tests1
 {
     public class SnakeGameViewModelTests
     {
@@ -69,7 +69,7 @@ namespace Tests1
 
             var newPosition = vm.SnakeParts.First();
 
-            
+
             Assert.True(vm.GameRunning);
         }
 
@@ -99,112 +99,112 @@ namespace Tests1
             Assert.Equal(dirBefore, vm.NextDirection);
         }
 
-    
-     
-            [Fact]
-            public void InitializeGame_ShouldSetInitialValues()
-            {
-                var model = new SnakeGameModel();
-                model.InitializeGame();
 
-                Assert.NotNull(model.SnakeParts);
-                Assert.NotEmpty(model.SnakeParts);
-                Assert.True(model.SnakeParts.Count >= 5);
-                Assert.Equal(new Vector(SnakeGameModel.SnakeSize, 0), model.SnakeDirection);
-                Assert.Equal(0, model.Score);
-            }
 
-            [Fact]
-            public void MoveSnake_ShouldChangeHeadPosition()
-            {
-                var model = new SnakeGameModel();
-                model.InitializeGame();
+        [Fact]
+        public void InitializeGame_ShouldSetInitialValues()
+        {
+            var model = new SnakeGameModel();
+            model.InitializeGame();
 
-                var initialHead = model.SnakeHeadPosition;
-                model.MoveSnake();
-                var newHead = model.SnakeHeadPosition;
+            Assert.NotNull(model.SnakeParts);
+            Assert.NotEmpty(model.SnakeParts);
+            Assert.True(model.SnakeParts.Count >= 5);
+            Assert.Equal(new Vector(SnakeGameModel.SnakeSize, 0), model.SnakeDirection);
+            Assert.Equal(0, model.Score);
+        }
 
-                Assert.NotEqual(initialHead, newHead);
-            }
+        [Fact]
+        public void MoveSnake_ShouldChangeHeadPosition()
+        {
+            var model = new SnakeGameModel();
+            model.InitializeGame();
 
-            [Fact]
-            public void AddSnakePart_ShouldIncreaseSnakeLength()
-            {
-                var model = new SnakeGameModel();
-                model.InitializeGame();
-                int oldLength = model.SnakeParts.Count;
+            var initialHead = model.SnakeHeadPosition;
+            model.MoveSnake();
+            var newHead = model.SnakeHeadPosition;
 
-                model.AddSnakePart();
+            Assert.NotEqual(initialHead, newHead);
+        }
 
-                Assert.Equal(oldLength + 1, model.SnakeParts.Count);
-            }
+        [Fact]
+        public void AddSnakePart_ShouldIncreaseSnakeLength()
+        {
+            var model = new SnakeGameModel();
+            model.InitializeGame();
+            int oldLength = model.SnakeParts.Count;
 
-            [Fact]
-            public void CreateFood_ShouldPlaceFoodOutsideSnake()
-            {
-                var model = new SnakeGameModel();
-                model.InitializeGame();
+            model.AddSnakePart();
 
-                Assert.DoesNotContain(model.SnakeParts, part => part == model.FoodPosition);
-            }
+            Assert.Equal(oldLength + 1, model.SnakeParts.Count);
+        }
 
-            [Fact]
-            public void CheckFoodCollision_ShouldGrowSnakeAndIncreaseScore()
-            {
-                var model = new SnakeGameModel();
-                model.InitializeGame();
-                model.FoodPosition = model.SnakeHeadPosition;
+        [Fact]
+        public void CreateFood_ShouldPlaceFoodOutsideSnake()
+        {
+            var model = new SnakeGameModel();
+            model.InitializeGame();
 
-                bool collided = model.CheckFoodCollision();
+            Assert.DoesNotContain(model.SnakeParts, part => part == model.FoodPosition);
+        }
 
-                Assert.True(collided);
-                Assert.Equal(10, model.Score);
-                Assert.Equal(6, model.SnakeParts.Count); 
-            }
+        [Fact]
+        public void CheckFoodCollision_ShouldGrowSnakeAndIncreaseScore()
+        {
+            var model = new SnakeGameModel();
+            model.InitializeGame();
+            model.FoodPosition = model.SnakeHeadPosition;
 
-            [Fact]
-            public void CheckCollisions_ShouldReturnTrue_WhenHitsWall()
-            {
-                var model = new SnakeGameModel();
-                model.InitializeGame();
+            bool collided = model.CheckFoodCollision();
 
-                model.SnakeHeadPosition = new Point(-10, 0); 
+            Assert.True(collided);
+            Assert.Equal(10, model.Score);
+            Assert.Equal(6, model.SnakeParts.Count);
+        }
 
-                Assert.True(model.CheckCollisions());
-            }
+        [Fact]
+        public void CheckCollisions_ShouldReturnTrue_WhenHitsWall()
+        {
+            var model = new SnakeGameModel();
+            model.InitializeGame();
 
-            [Fact]
-            public void CheckCollisions_ShouldReturnTrue_WhenHitsItself()
-            {
-                var model = new SnakeGameModel();
-                model.InitializeGame();
+            model.SnakeHeadPosition = new Point(-10, 0);
 
-                // Принудительно замыкаем змейку на саму себя
-                model.SnakeHeadPosition = model.SnakeParts[3];
+            Assert.True(model.CheckCollisions());
+        }
 
-                Assert.True(model.CheckCollisions());
-            }
+        [Fact]
+        public void CheckCollisions_ShouldReturnTrue_WhenHitsItself()
+        {
+            var model = new SnakeGameModel();
+            model.InitializeGame();
+
+            // Принудительно замыкаем змейку на саму себя
+            model.SnakeHeadPosition = model.SnakeParts[3];
+
+            Assert.True(model.CheckCollisions());
+        }
 
         [Fact]
         public void Execute_ShouldCallAction()
         {
-            
+
             bool wasExecuted = false;
             var command = new RelayCommand(_ => wasExecuted = true);
 
             command.Execute(null);
 
-         
+
             Assert.True(wasExecuted);
         }
 
         [Fact]
         public void CanExecute_ShouldReturnTrue_WhenPredicateIsNull()
         {
-        
+
             var command = new RelayCommand(_ => { });
 
-            
+
             bool result = command.CanExecute(null);
 
             Assert.True(result);
@@ -215,20 +215,20 @@ namespace Tests1
         [InlineData(false)]
         public void CanExecute_ShouldRespectPredicate(bool expected)
         {
-            
+
             var command = new RelayCommand(_ => { }, _ => expected);
 
-          
+
             bool result = command.CanExecute(null);
 
-            
+
             Assert.Equal(expected, result);
         }
 
         [Fact]
         public void CanExecuteChanged_ShouldAttachAndDetachHandlers()
         {
-            
+
             var command = new RelayCommand(_ => { });
             EventHandler handler = (_, _) => { };
 
